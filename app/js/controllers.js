5794e2f259a0d55757cfe04bef14ab7402b9ea19
admin <rohit@ubuntu.ubuntu-domain>
2013-03-19 19:10:30 +0530

    working code

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,81 +3,88 @@
 /* Controllers */
 
 function EntityCtrl($scope) {
-	var ids = [];
-	for (var i = 0; i < 5; i++) {
-		var id = Math.random();
-		ids[i] = id;
-	}
-	console.log(ids);
 	$scope.types = ["Text", "Checkbox", "Radio", "Textarea", "Collection"];
+	$scope.field = {
+		name : 'root',
+		type : 'Collection',
+		childs : []
+	};
 
-	$scope.fields = [{
-		id : ids[0],
-		name : '',
-		type : 'Text',
-		children : []
-	}, {
-		id : ids[1],
-		name : '',
-		type : 'Text',
-		children : []
-	}, {
-		id : ids[2],
-		name : '',
-		type : 'Text',
-		children : []
-	}, {
-		id : ids[3],
-		name : '',
-		type : 'Text',
-		children : []
-	}, {
-		id : ids[4],
-		name : '',
-		type : 'Text',
-		children : []
-	}];
-
-	$scope.addMore = function(fields) {
-		id = Math.random();
-		ids.push(id);
-		fields.push({
-			id : id,
-			name : '',
-			type : 'Text',
-			children : []
-		});
-	}
-
-	$scope.addChildren = function(field) {
-		id = Math.random();
-		ids.push(id);
+	$scope.add_child = function(parent_node) {
 		var child_node = {
-			id : id,
 			name : '',
-			type : 'Text',
-			children : []
+			type : 'TEXT',
+			childs : []
 		};
-		field.children.push(child_node);
 
-	}
+		if (parent_node.childs) {
+			parent_node.childs.push(child_node);
+		} else {
+			parent_node.childs = [child_node];
+		}
+
+	};
+
 }
 
-// declare a module
+/*function EntityCtrl($scope) {
+ var ids = [];
+ for (var i = 0; i < 5; i++) {
+ var id = Math.random();
+ ids[i] = id;
+ }
+ console.log(ids);
+ $scope.types = ["Text", "Checkbox", "Radio", "Textarea", "Collection"];
 
-var myAppModule = angular.module('myApp', []);
+ $scope.fields = [{
+ id : ids[0],
+ name : '',
+ type : 'Text',
+ children : []
+ }, {
+ id : ids[1],
+ name : '',
+ type : 'Text',
+ children : []
+ }, {
+ id : ids[2],
+ name : '',
+ type : 'Text',
+ children : []
+ }, {
+ id : ids[3],
+ name : '',
+ type : 'Text',
+ children : []
+ }, {
+ id : ids[4],
+ name : '',
+ type : 'Text',
+ children : []
+ }];
 
-// configure the module.
-// in this example we will create a greeting filter
+ $scope.addMore = function(fields) {
+ id = Math.random();
+ ids.push(id);
+ fields.push({
+ id : id,
+ name : '',
+ type : 'Text',
+ children : []
+ });
+ }
 
-myAppModule.filter('greet', function() {
-	return function(name) {
-		return 'Hello, ' + name + '!';
-	};
-}); 
+ $scope.addChildren = function(field) {
+ id = Math.random();
+ ids.push(id);
+ var child_node = {
+ id : id,
+ name : '',
+ type : 'Text',
+ children : []
+ };
+ field.children.push(child_node);
 
-
-
-
-
-
+ }
+ }
+ */
